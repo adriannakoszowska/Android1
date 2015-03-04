f0bca72ee751e7ec7ea8dbad6bb3173a9336657d
Adrianna Koszowska <adriannakoszowska@gmail.com>
2015-03-04 21:18:37 +0100

    wczytanie obiektów, czyszczenie ich (wyświetlaczy)

--- app/src/main/java/com/example/adriannakoszowska/android1/MainActivity.java
+++ app/src/main/java/com/example/adriannakoszowska/android1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // wczytanie obiektów
+        TextView poprzednie=(TextView)findViewById(R.id.poprzednie);
+        TextView dzialanie=(TextView)findViewById(R.id.dzialanie);
+        // ustawienie tekstu na obiektach
+        poprzednie.setText("");
+        dzialanie.setText("");
     }
 
 
